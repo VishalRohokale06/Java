ef5c4307fff14763092e919c6ef8dfbf3fc17eae
VishalRohokale06 <vvrohokale16@gmail.com>
2025-12-03 10:27:40 +0530

    dsa practice code

---
+++ JAVA/dsac2w/code15.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Demo{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter String:");
+		String str = sc.nextLine();
+
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--){
+			rev+=str.charAt(i);
+		}
+		System.out.println(rev);
+		if(rev.equals(str)){
+			System.out.println("String is Palidrom");
+		}else{
+			System.out.println("String is not palidrom");
+		}
+	}
+}
