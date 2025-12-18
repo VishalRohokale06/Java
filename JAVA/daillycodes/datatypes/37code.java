a9417cd766061fd9f22a192ef5658fcf875592b4
VishalRohokale06 <vvrohokale16@gmail.com>
2025-12-18 14:07:43 +0530

    datatypes code

---
+++ JAVA/daillycodes/datatypes/37code.java
@@ -0,0 +1,8 @@
+class C2W_CharIntDemo{
+        public static void main(String[] args){
+                char valueFirst='0';
+                char valueSecond='1';
+                System.out.println(valueFirst);
+                System.out.println(valueSecond);
+        }
+}
