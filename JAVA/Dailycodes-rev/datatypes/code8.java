350d37610698c052d41f0d991b30439634855bed
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-02 21:39:50 +0530

    datatype code

---
+++ JAVA/Dailycodes-rev/datatypes/code8.java
@@ -0,0 +1,13 @@
+class C2W_CharDemo{
+	public static void main(String args[]){
+		int ch = 65;
+		//char ch1 = ch;
+		char ch2 = 68;
+
+                System.out.println(ch);
+                //System.out.priintln(ch1);
+                System.out.println(ch2);
+
+	
+	}
+}
