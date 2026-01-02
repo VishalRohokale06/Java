4b3bc3fe5b9dffb507e0681309215532734a287f
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-02 21:40:12 +0530

    datatype code

---
+++ JAVA/Dailycodes-rev/datatypes/code10.java
@@ -0,0 +1,7 @@
+class C2W_Boolemo{
+        public static void main(String[] args){
+                boolean ch1 = true;
+                boolean ch2 = false;
+                System.out.println(ch1);
+        }
+}
