60b19015aeabce10c2ea9891725606b0d6c296d3
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-02 21:41:19 +0530

    datatype code

---
+++ JAVA/Dailycodes-rev/datatypes/code13.java
@@ -0,0 +1,7 @@
+class C2W_CharDemo{
+        public static void main(String[] args){
+                char ch1 = 1;
+                char ch2 = 65;
+                System.out.println(ch2);
+        }
+}
