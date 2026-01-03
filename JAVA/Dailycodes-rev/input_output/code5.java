bf1b63f901b2c59c18bfa841debcd8c4ed92cfed
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-03 11:34:12 +0530

    Input-Output code

---
+++ JAVA/Dailycodes-rev/input_output/code5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Demo{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		String name = sc.next();
+		String clgName = sc.next();
+		int id = sc.nextInt();
+		float cgpa = sc.nextFloat();
+
+
+		System.out.println(name);
+		System.out.println(clgName);
+		System.out.println(id);
+		System.out.println(cgpa);
+	}
+}
