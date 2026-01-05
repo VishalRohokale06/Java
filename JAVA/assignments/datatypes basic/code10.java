0ce9f4d8365c06a068acc8f0b8eef2a2a868c709
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-05 14:06:45 +0530

    datatypes code

---
+++ JAVA/assignments/datatypes basic/code10.java
@@ -0,0 +1,10 @@
+class Demo{
+        public static void main(String[] args){
+
+                String coll = "36.2cr";
+                float rating = 4.4f; 
+
+                System.out.println("box office collection :"+coll);                
+		System.out.println("imbd rating :"+rating);
+	}
+}
