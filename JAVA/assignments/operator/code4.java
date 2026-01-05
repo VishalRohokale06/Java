8dce7a64f664678c429064ede9710158d0847972
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-05 14:08:40 +0530

    operator code

---
+++ JAVA/assignments/operator/code4.java
@@ -0,0 +1,16 @@
+class Demo{
+	public static void main(String[] args){
+
+		boolean a = true;
+		boolean b = false;
+
+		//if(a && b)
+			System.out.println("(a&&b) ="+(a&&b));
+		//else if(a || b)
+			System.out.println("a||b ="+(a||b));
+		//else if(!a)
+			System.out.println("!a ="+!a);
+		//else
+			System.out.println("!b ="+b);
+	}
+}
