1eb81547e3d37ad05c757c918871ec81c24d1a8e
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-15 19:03:56 +0530

    space_pattern code

---
+++ JAVA/practicles/IO/space_pattern::18/code7.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Demo{
+
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Rows=");
+		int rows=sc.nextInt();
+
+		//int num = 1;
+
+		for(int i=rows;i>0;i--){
+			for(int space=rows;space>i;space--){
+				System.out.print(" "+" ");
+			}
+		        int num = 1;
+			for(int j=1;j<=i;j++){
+				System.out.print(num+" ");
+				num++;
+			}
+			System.out.println();
+		}
+	}
+}
+
