a509b1f9acadd71006bbede096a4d1cb9bd6733b
VishalRohokale06 <vvrohokale16@gmail.com>
2026-01-15 19:06:26 +0530

    triangle_pattern code

---
+++ JAVA/practicles/IO/trianglepattern13/code7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Demo{
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+
+                System.out.print("Rows =");
+                int row = sc.nextInt();
+		int num=row;
+		int ch=63+row;
+
+		for(int i=row;i>0;i--){
+			for(int j=1;j<=i;j++){
+
